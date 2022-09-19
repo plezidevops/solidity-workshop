e824def92ae4f1b7a49c26c42af78f0c1f8ed80e
plezidevops <plezidevops@gmail.com>
2022-09-19 09:04:15 -0400

    greeting contract

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.9;
 
 contract MyContract {
     string public greetings = "Hello world";
+
+    function updateOurString(string memory _greetings) public {
+        greetings = _greetings;
+    }
 }
